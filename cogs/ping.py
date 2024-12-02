5e5ccaf21cd90ad1b295ab845088a7a0d730b3e9
Tacordack <tacordack@gmail.com>
2024-12-02 21:47:57 +0330

    update: cogs/ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -8,12 +8,32 @@ class ping(commands.Cog):
 
         self.bot = bot
 
-    @nextcord.slash_command(name='ping',description='Pong')
+    @nextcord.slash_command(name='ping',description='نشون دادن پینگ')
     async def ping(self,interaction:Interaction):
+
+        colors = {
+            "green": nextcord.Color.green(),
+            "yellow": nextcord.Color.yellow(),
+            "red": nextcord.Color.red()
+        }
+
+        color = None
 
         ping = round(self.bot.latency*1000)
 
-        embed = nextcord.Embed(title=':ping_pong: Pong!',description=f':wireless: {ping} ms',color=nextcord.Color.blue())
+        if ping <= 100:
+
+            color = colors["green"]
+        
+        elif ping > 101 and ping < 300:
+
+            color = colors["yellow"]
+
+        elif ping > 300:
+
+            color = colors["red"]
+            
+        embed = nextcord.Embed(title=':ping_pong: پنگ',description=f':wireless: پینگ شما {ping} است',color=color)
 
         await interaction.response.send_message(embed=embed)
 
